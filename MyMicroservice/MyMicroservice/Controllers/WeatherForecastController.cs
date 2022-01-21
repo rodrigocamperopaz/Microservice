3ea5b2b6358d3a858128b72f20ab1ef3abc41f90
Jorge Zientarski <jorge.zientarski@assuresoftbolivia.com>
2022-01-20 22:24:14 -0400

    Added Weather API

--- MyMicroservice/MyMicroservice/Controllers/WeatherForecastController.cs
+++ MyMicroservice/MyMicroservice/Controllers/WeatherForecastController.cs
@@ -1,4 +1,9 @@
 using Microsoft.AspNetCore.Mvc;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace MyMicroservice.Controllers
 {
@@ -28,6 +33,34 @@ namespace MyMicroservice.Controllers
                 Summary = Summaries[Random.Shared.Next(Summaries.Length)]
             })
             .ToArray();
+        }
+
+        [HttpGet("[action]/{city}")]
+        public async Task<IActionResult> City(string city)
+        {
+            using (var client = new HttpClient())
+            {
+                try
+                {
+                    string token = "37d835d227d9d920203bbdc1765f3cb1";
+                    client.BaseAddress = new Uri("http://api.openweathermap.org");
+                    var response = await client.GetAsync($"/data/2.5/weather?q={city}&appid={token}&units=metric");
+                    response.EnsureSuccessStatusCode();
+
+                    var stringResult = await response.Content.ReadAsStringAsync();
+                    var rawWeather = JsonConvert.DeserializeObject<OpenWeatherResponse>(stringResult);
+                    return Ok(new
+                    {
+                        Temp = rawWeather.Main.Temp,
+                        Summary = string.Join(",", rawWeather.Weather.Select(x => x.Main)),
+                        City = rawWeather.Name
+                    });
+                }
+                catch (HttpRequestException httpRequestException)
+                {
+                    return BadRequest($"Error getting weather from OpenWeather: {httpRequestException.Message}");
+                }
+            }
         }
     }
 }
--- MyMicroservice/MyMicroservice/WeatherForecast.cs
+++ MyMicroservice/MyMicroservice/WeatherForecast.cs
@@ -10,4 +10,24 @@ namespace MyMicroservice
 
         public string? Summary { get; set; }
     }
+
+    public class OpenWeatherResponse
+    {
+        public string Name { get; set; }
+
+        public IEnumerable<WeatherDescription> Weather { get; set; }
+
+        public Main Main { get; set; }
+    }
+
+    public class WeatherDescription
+    {
+        public string Main { get; set; }
+        public string Description { get; set; }
+    }
+
+    public class Main
+    {
+        public string Temp { get; set; }
+    }
 }
